647616b2b87005ff0313b97b9a9be8bfcf0af00e
ashutoshroy <ashutoshroywork@gmail.com>
2022-10-14 07:46:34 +0530

    [Ashutosh] Initialize Spring Boot Project.

---
+++ src/main/java/com/hobbyprojects/tinkeringwithcode/TinkeringwithcodeApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyprojects.tinkeringwithcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkeringwithcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkeringwithcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobbyprojects/tinkeringwithcode/TinkeringwithcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyprojects.tinkeringwithcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkeringwithcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
