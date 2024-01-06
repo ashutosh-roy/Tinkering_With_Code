6880f17e374667f445929a00be9807ae89bfab42
ashutosh-roy <ashutoshroywork@gmail.com>
2024-01-06 18:05:21 +0530

    TWC-1 Add tests for main method.

--- src/test/java/com/hobbyprojects/tinkeringwithcode/TinkeringwithcodeApplicationTests.java
+++ src/test/java/com/hobbyprojects/tinkeringwithcode/TinkeringwithcodeApplicationTests.java
@@ -1,13 +1,26 @@
 package com.hobbyprojects.tinkeringwithcode;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class TinkeringwithcodeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void testApplication() {
+        try (final MockedStatic<SpringApplication> utilities = mockStatic(SpringApplication.class)) {
+            utilities.when((MockedStatic.Verification) SpringApplication.run(TinkeringwithcodeApplication.class)).thenReturn(null);
+            TinkeringwithcodeApplication.main(new String[]{});
+            assertNull(SpringApplication.run(TinkeringwithcodeApplication.class));
+        }
+    }
 }
