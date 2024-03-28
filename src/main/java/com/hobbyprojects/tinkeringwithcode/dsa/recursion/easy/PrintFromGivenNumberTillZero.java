bf2c15dbe92e35cddd9b199e159d519e7cafe67e
ashutosh-roy <ashutoshroywork@gmail.com>
2024-03-28 14:18:46 +0530

    LC-1 Recursion Basics.

---
+++ src/main/java/com/hobbyprojects/tinkeringwithcode/dsa/recursion/easy/PrintFromGivenNumberTillZero.java
@@ -0,0 +1,24 @@
+package com.hobbyprojects.tinkeringwithcode.dsa.recursion.easy;
+
+import java.util.Scanner;
+
+/**
+ * <h3>Recursion</h3>identify the base condition first and then write the logic.
+ */
+public class PrintFromGivenNumberTillZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n :-");
+        int n = sc.nextInt();
+        printFromGivenNumberTillZero(n);
+    }
+
+    static void printFromGivenNumberTillZero(int n) {
+        System.out.println(n);
+        n = n - 1;
+        if (n < 0) {
+            return;
+        }
+        printFromGivenNumberTillZero(n);
+    }
+}
